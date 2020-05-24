e20009cdf5c783b9b6ef5d4968dfc6c16a7751c7
JNA <josiah.amanna@niveussolutions.com>
2020-05-24 15:48:35 +0530

    Aded generate token test and code refactored

--- _test_/server/models/user.spec.js
+++ _test_/server/models/user.spec.js
@@ -5,8 +5,18 @@
 import User from "@models/User";
 import Bcrypt from "bcryptjs";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import config from "@config";
 
 describe("The user model", () => {
+  const user = {
+    name: "Test User",
+    email: "test@user.com",
+    password: "Testpassword",
+  };
+
+  let CreatedUser;
+
   beforeAll(async () => {
     const conn = await mongoose.connect(
       "mongodb://localhost:27017/auth-app_test",
@@ -14,31 +24,28 @@ describe("The user model", () => {
         useNewUrlParser: true,
       }
     );
+
+    CreatedUser = await User.create(user);
   });
 
   it("Should hash the password before saving to the database", async () => {
-    const user = {
-      name: "Test User",
-      email: "test@user.com",
-      password: "Testpassword",
-    };
-
-    const CreatedUser = await User.create(user);
-
     expect(Bcrypt.compareSync(user.password, CreatedUser.password)).toBe(true);
   });
 
   it("Should set the email confirm code for the user before saving the database", async () => {
-    const user = {
-      name: "Test User",
-      email: "test@user.com",
-      password: "Testpassword",
-    };
-
-    const CreatedUser = await User.create(user);
-
     expect(CreatedUser.emailConfirmCode).toEqual(expect.any(String));
   });
+
+  describe("Generate Token Method", () => {
+    it("Should generate a valid jwt for a user", () => {
+      const token = CreatedUser.generateToken();
+      const { id } = jwt.verify(token, config.jwtSecret);
+      expect(id).toEqual(JSON.parse(JSON.stringify(CreatedUser._id)));
+      // alternative way of stringify
+      expect(id).toEqual(String(CreatedUser._id));
+    });
+  });
+
   afterAll(async () => {
     await mongoose.connection.close();
   });
